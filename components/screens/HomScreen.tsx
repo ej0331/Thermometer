73eb0ea68a4b4cadc528378c6718375e4fb51755
ej0331 <k0970133227@gmail.com>
2023-02-06 17:17:57 +0800

    install package

---
+++ components/screens/HomScreen.tsx
@@ -0,0 +1,13 @@
+import { Text, Image, View, StyleSheet, TouchableOpacity, TextInput, ScrollView, FlatList, Button } from "react-native";
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>
+                This is HomeScreen.
+            </Text>
+        </View>
+    )
+}
+
+export default HomeScreen
